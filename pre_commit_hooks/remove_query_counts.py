43a5fb7cff1216c54b13b6c95cd354b0a24d4a0b
aa512 <sondre.gundersen@intility.no>
2020-09-03 17:58:50 +0200

    Testing

--- pre_commit_hooks/remove_query_counts.py
+++ pre_commit_hooks/remove_query_counts.py
@@ -4,15 +4,15 @@ from typing import Optional, Sequence
 
 
 def _process_line(line: bytes) -> bytes:
-    if b'@db_helper()  #' in line:
+    if line.startswith(b'@db_helper()  #'):
         return b'@db_helper()\n'
     return line
 
 
-def _fix_file(filename: str, chars: Optional[bytes]) -> bool:
+def _fix_file(filename: str) -> bool:
     with open(filename, mode='rb') as file_processed:
         lines = file_processed.readlines()
-    newlines = [_process_line(line, chars) for line in lines]
+    newlines = [_process_line(line) for line in lines]
     if newlines != lines:
         with open(filename, mode='wb') as file_processed:
             for line in newlines:
@@ -25,11 +25,10 @@ def _fix_file(filename: str, chars: Optional[bytes]) -> bool:
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     args = parser.parse_args(argv)
-    chars = None if args.chars is None else args.chars.encode()
     return_code = 0
     for filename in args.filenames:
         _, extension = os.path.splitext(filename.lower())
-        if _fix_file(filename, chars):
+        if _fix_file(filename):
             print(f'Fixing {filename}')  # noqa: T001
             return_code = 1
     return return_code
